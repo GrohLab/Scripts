71edb35c63c0f2577c3c1c5b862aa29b2b12373f
rivkahrivkah <beckin@gmail.com>
2020-01-15 18:15:05 +0100

    Update dPCAscratch_ram.m
    figuring out animations

--- Jesus/dPCAscratch_ram.m
+++ Jesus/dPCAscratch_ram.m
@@ -1,44 +1,61 @@
 %% try dpca with jittering data
+
+%Z:\Jesus\Jittering\Already Curated\190619_Jesus_Emilio_Jittering_3703_1500_1520
+
 clear all
-
-dataDir='\\lsdf02.urz.uni-heidelberg.de\sd19B001\Jesus\Jittering\190702_Jittering_3720_1520_1520'
-
-%'\\lsdf02.urz.uni-heidelberg.de\sd19B001\Jesus\Jittering\190712_Jesus_Emilio_Jittering_3700_1520_1500'
-
-ls
-
+dataDir='Z:\Jesus\Jittering\Already Curated\190702_Jittering_3720_1520_1520'
 cd(dataDir)
 load('jesus_emilio_jittering1_PopulationSpikeAnalysis.mat')
 load('jesus_emilio_jittering1_all_channels.mat')
 load(['jesus_emilio_jittering1analysis.mat'],'Triggers')
 
+%% another example with gamma
+clear all
+dataDir='\\lsdf02.urz.uni-heidelberg.de\sd19B001\Jesus\Jittering\Already Curated\190712_Jesus_Emilio_Jittering_3700_1520_1500'
+cd(dataDir)
+load('Jitter_PopulationSpikeAnalysis.mat')
+load('Jitter_all_channels.mat')
+load(['Jitteranalysis.mat'],'Triggers')
+
+
+
+%% another example
+clear all
+dataDir='Z:\Jesus\Jittering\Already Curated\190703_Jittering_3720_1520_1520'
+cd(dataDir)
+%load('Jitter_PopulationSpikeAnalysis.mat')
+load('H5_1.3mW_all_channels.mat')
+load(['H5_1.3mWanalysis.mat'],'Triggers', 'Conditions')
 %% make it simple and find everything 
-% well isolated clusters
-goodsInd = cellfun(@(x) x==1,sortedData(:,3));
-goods = find(goodsInd);
+% well isolated clusters, only isolated
+goodsInd = cellfun(@(x) x==1,sortedData(:,3));multiInd=[];
+%multiInd = cellfun(@(x) x==2,sortedData(:,3));
+goods = [find(goodsInd); find(multiInd)];
 Ns = min(structfun(@numel,Triggers));
 % Total duration of the recording
 Nt = Ns/fs;  %seconds
 clusters=sortedData(goods,2);ids=sortedData(goods,1);
 %clusters = cellfun(@(x) round(x*fs/1000),clusters,'UniformOutput',false);  %spikes now at ms resolution
-
-biSpikes=spalloc(floor(Nt*1000),numel(goods),numel(cell2mat(clusters)));% preallocate sparse matrix to save space
+ppms=1;
+biSpikes=spalloc(floor(Nt*1000*ppms),numel(goods),numel(cell2mat(clusters)));% preallocate sparse matrix to save space
 % ms x num clusters
 T=size(biSpikes,1); %recording length in ms
 
-stimCond=Conditions(3:end);
-stimCond(end-1).Triggers=stimCond(end-1).Triggers(1:5:end-1,:)
+
+
+stimCond=Conditions([3:9]);
+%stimCond(end-1).Triggers=stimCond(end-1).Triggers(1:5:end-1,:)
 %make big matrix of all spike times
 for j=1:numel(goods)
-    tt=round(clusters{j}*1000);tt=tt(tt>0 & tt<=T);%spike times in ms
+    tt=round(clusters{j}*1000*ppms);tt=tt(tt>0 & tt<=T);%spike times in ms*ppms
     S=zeros(T,1);S(tt)=1;
     biSpikes(:,j)=sparse(S);
 end
-
+%%
 maxTrials=max(cellfun(@(x) size(x,1),{stimCond.Triggers}));
 %in ms because of division before, though triggered seg takes samples
-timeBefore=-600
-timeAfter=700
+timeBefore=-500*ppms;
+timeAfter=500*ppms;
 trialLength=-timeBefore+timeAfter+1;
 
 %preallocate omg could be huge!
@@ -46,9 +63,9 @@ trialLength=-timeBefore+timeAfter+1;
 firingRates=zeros(numel(clusters),numel(stimCond),trialLength,maxTrials);
 for n =1:numel(clusters)
     for s=1:numel(stimCond)
-        triggers=round(stimCond(s).Triggers(:,1)/fs*1000); %triggers in ms
+        triggers=round(stimCond(s).Triggers(:,1)/fs*1000*ppms); %triggers in ms*ppms
         X=TriggeredSegments(biSpikes(:,n),triggers,timeBefore,timeAfter);
-        firingRates(n,s,:,:)=(shiftdim(X,-2));  %some fuckery to deal with 4d array
+        firingRates(n,s,1:size(X,1),1:size(X,2))=(shiftdim(X,-2));  %some fuckery to deal with 4d array
     end
   
 end
@@ -60,7 +77,7 @@ trialNum=ones(numel(clusters), numel(stimCond))*maxTrials;
 firingRatesAverage=mean(firingRates,4);
 
 time = 1:trialLength;
-%%
+%
 
     %bin triggered spikes for each neuron (use reshape business from JP)
     %squish into firingRates
@@ -81,7 +98,7 @@ time = 1:trialLength;
     
    
     
-%% Define parameter grouping
+% Define parameter grouping
 
 % *** Don't change this if you don't know what you are doing! ***
 % firingRates array has [N S D T E] size; herewe ignore the 1st dimension 
@@ -134,7 +151,7 @@ ifSimultaneousRecording = true;  % change this to simulate simultaneous
                                  % recordings (they imply the same number 
                                  % of trials for each neuron)
 
-%% Step 1: PCA of the dataset
+% Step 1: PCA of the dataset
 close all
 X = firingRatesAverage(:,:);
 X = bsxfun(@minus, X, mean(X,2));
@@ -158,12 +175,12 @@ dpca_plot(firingRatesAverage, W, W, @dpca_plot_default, ...
     'marginalizationColours', margColours);
 
 
-%% Step 2: PCA in each marginalization separately
+% Step 2: PCA in each marginalization separately
 
 dpca_perMarginalization(firingRatesAverage, @dpca_plot_default, ...
    'combinedParams', combinedParams);
 
-%% Step 3: dPCA without regularization and ignoring noise covariance
+% Step 3: dPCA without regularization and ignoring noise covariance
 
 % This is the core function.
 % W is the decoder, V is the encoder (ordered by explained variance),
@@ -190,59 +207,7 @@ dpca_plot(firingRatesAverage, W, V, @dpca_plot_default, ...
 
 
 
-
-%%
-% ------------- 3D visualization ------------
-X = firingRatesAverage(n,:,:,t);
-X = X(:,:);
-X = bsxfun(@minus, X, mean(X,2));
-Z = W(:,whichMarg==1)'*X;
-Zfull = reshape(Z(1:3,:), [3 size(firingRatesAverage,2) size(firingRatesAverage,3) length(t)]);
-colors = jet(7);
-colors = colors([1 2 4 5 6 7],:);
-colors(3,:) = colors(3,:)*0.6;
-colors(4,:) = [238 210 2]/256;
-ZZ = mean(Zfull, 3);
-t1 = [41:110];
-t2 = [391:460];
-
-figure('Position', [2000 0 1300 1300])
-axes('Units', 'Pixels', 'Position', [1 1 1300 1300], 'Color', [0 0 0])
-hold on
-for i=1:6
-    plot3(squeeze(ZZ(1,i,:)), squeeze(ZZ(2,i,:)), squeeze(ZZ(3,i,:)), 'LineWidth', 2, 'Color', colors(i,:))
-    plot3(squeeze(ZZ(1,i,t1)), squeeze(ZZ(2,i,t1)), squeeze(ZZ(3,i,t1)), 'LineWidth', 4, 'Color', colors(i,:))
-    plot3(squeeze(ZZ(1,i,t2)), squeeze(ZZ(2,i,t2)), squeeze(ZZ(3,i,t2)), 'LineWidth', 4, 'Color', colors(i,:))
-    plot3(squeeze(ZZ(1,i,t2(1:2:end))), squeeze(ZZ(2,i,t2(1:2:end))), squeeze(ZZ(3,i,t2(1:2:end))), '.', ...
-        'MarkerSize', 25, 'Color', colors(i,:))
-end
-axis normal
-axis(110*[-1 1 -1 1 -1 1])
-axis vis3d
-
-vidObj = VideoWriter('movie_romo.avi');
-open(vidObj);
-thetas = 1:1:360;
-for i = 1:length(thetas)
-    view([cosd(-135+thetas(i)) sind(-135+thetas(i)) 0.2])
-    pause(0.01)
-    thisFrame = getframe(gca);
-    thisFrame.cdata = thisFrame.cdata(1:1300,1:1300,:);
-    writeVideo(vidObj, thisFrame);
-end
-close(vidObj);
-
-figure('Position', [2000 0 1300 1300])
-axes('Units', 'Pixels', 'Position', [1 1 1300 1300], 'Color', [0 0 0])
-hold on
-axis(110*[-1 1 -1 1])
-cols = [];
-for i=1:6
-    cols = [cols; linspace(colors(i,1),0,30)' linspace(colors(i,2),0,30)' linspace(colors(i,3),0,30)'];
-end
-colormap(cols)
-
-%% Step 4: dPCA with regularization
+% Step 4: dPCA with regularization
 
 % This function takes some minutes to run. It will save the computations 
 % in a .mat file with a given name. Once computed, you can simply load 
@@ -279,6 +244,174 @@ dpca_plot(firingRatesAverage, W, V, @dpca_plot_default, ...
     'timeEvents', timeEvents,               ...
     'timeMarginalization', 3,           ...
     'legendSubplot', 16);
+
+
+
+%% visualize
+n=1:size(firingRates,1);
+t = [1:length(time)]'; %time points of interest
+X = firingRatesAverage(n,:,t);
+X = X(:,:);
+X = bsxfun(@minus, X, mean(X,2));
+Z = W(:,whichMarg==1)'*X;gi
+Zfull = reshape(Z(1:3,:), [3 size(firingRatesAverage,2) length(t)]);
+ZZ = mean(Zfull, 3);
+ZZ = Zfull;  %I THINK??? as we are missing a dimension
+
+
+
+      
+i=7;
+id=ones(numel(t),1);
+controlw=[squeeze(ZZ(1,i,:)) squeeze(ZZ(2,i,:)) squeeze(ZZ(3,i,:)) t id*i];
+
+for i=1:5
+    str=['lag' num2str(i) '=[squeeze(ZZ(1,i,:)) squeeze(ZZ(2,i,:)) squeeze(ZZ(3,i,:)) t  id*i];'];
+    eval(str);
+end
+
+
+src = cat(1, controlw, lag1, lag2, lag3, lag4, lag5);
+%make pleasant colors
+[colormap]=cbrewer('seq', 'BuPu', 5)
+cmap=repmat([1 0 0], numel(t),1);
+for i=1:5
+    cmap=[cmap; repmat(colormap(i,:), numel(t),1)];
+end
+
+src =[src brighten(cmap,0)];
+comet3n(src, 'speed', 10, 'headsize', 2, 'tailwidth', 1.5, 'taillength', 50)
+%%
+% ------------- 3D visualization ------------
+
+X = firingRatesAverage(n,:,t);
+X = X(:,:);
+X = bsxfun(@minus, X, mean(X,2));
+Z = W(:,whichMarg==1)'*X;
+Zfull = reshape(Z(1:3,:), [3 size(firingRatesAverage,2) length(t)]);
+colors = jet(8);
+colors = colors([1 2 4 5 6 7 8 ],:);
+colors(3,:) = colors(3,:)*0.6;
+colors(4,:) = [238 210 2]/256;
+ZZ = mean(Zfull, 3);
+ZZ = Zfull;  %I THINK??? as we are missing a dimension
+t1 = [41:110];
+t2 = [391:460];
+
+figure('Position', [2000 0 1300 1300])
+axes('Units', 'Pixels', 'Position', [1 1 1300 1300], 'Color', [0 0 0])
+hold on
+for i=1:6 %for each condition
+    plot3(squeeze(ZZ(1,i,:)), squeeze(ZZ(2,i,:)), squeeze(ZZ(3,i,:)), 'LineWidth', 2, 'Color', colors(i,:))
+    plot3(squeeze(ZZ(1,i,t1)), squeeze(ZZ(2,i,t1)), squeeze(ZZ(3,i,t1)), 'LineWidth', 4, 'Color', colors(i,:))
+    plot3(squeeze(ZZ(1,i,t2)), squeeze(ZZ(2,i,t2)), squeeze(ZZ(3,i,t2)), 'LineWidth', 4, 'Color', colors(i,:))
+    plot3(squeeze(ZZ(1,i,t2(1:2:end))), squeeze(ZZ(2,i,t2(1:2:end))), squeeze(ZZ(3,i,t2(1:2:end))), '.', ...
+        'MarkerSize', 25, 'Color', colors(i,:))
+end
+axis normal
+axis(110*[-1 1 -1 1 -1 1])
+axis vis3d
+
+
+%%
+t2=t;
+t1=t;
+figure
+    grid on
+for i=[6 7 1:5] %for each condition
+    plot3(squeeze(ZZ(1,i,:)), squeeze(ZZ(2,i,:)),squeeze(ZZ(3,i,:)),'.-', 'LineWidth', 2, 'Color', colors(i,:))
+    %plot3(squeeze(ZZ(1,i,:)), squeeze(ZZ(2,i,:)),squeeze(ZZ(3,i,:)),'.-', 'MarkerSize', 10, 'Color', colors(i,:))
+
+    grid on
+    xlabel dpca1
+    ylabel dpca2
+    zlabel dpca3
+%set(gca,'Zlim',zlims)
+%set(gca,'Xlim',xlims)
+%set(gca,'Ylim',ylims)
+hold on
+    pause
+end
+
+
+%%
+zlims=get(gca,'Zlim')
+xlims=get(gca,'Xlim')
+ylims=get(gca,'Ylim')
+
+
+%%
+plot(squeeze(ZZ(1,i,t1)), squeeze(ZZ(2,i,t1)), 'LineWidth', 4, 'Color', colors(i,:))
+plot(squeeze(ZZ(1,i,t2)), squeeze(ZZ(2,i,t2)), 'LineWidth', 4, 'Color', colors(i,:))
+plot(squeeze(ZZ(1,i,t2(1:2:end))), squeeze(ZZ(2,i,t2(1:2:end))), '.', ...
+        'MarkerSize', 25, 'Color', colors(i,:))
+
+
+    
+
+    
+%% attempting to make a movie!
+pca1=squeeze(ZZ(1,i,:));
+pca2=squeeze(ZZ(2,i,:));
+X=pca1
+Y=nan(numel(X),1,numel(t));
+Y(:,1,:)=pca2;
+[hL] = animator(pca1,pca2,'ylim','auto','title','dPCA','smooth','on');
+animator(pca1,pca2)
+%%
+    %plot3, ,squeeze(ZZ(3,i,:)),'.-', 'LineWidth', 2, 'Color', colors(i,:))
+    
+i=7;
+controlw=[squeeze(ZZ(1,i,:)) squeeze(ZZ(2,i,:)) squeeze(ZZ(3,i,:))]
+i=1
+lag1=[squeeze(ZZ(1,i,:)) squeeze(ZZ(2,i,:)) squeeze(ZZ(3,i,:))]
+all={};all{1}=controlw;all{2}=lag1;
+plot_coords(all);
+      trajectory_plotter(['test'], 10, controlw,lag1);
+      
+  
+
+      
+      %%
+      
+%%
+     
+  %plot 3 5-D random walks, each in a different color.  Save the output to
+  %'trajectories_test.avi' in the current working directory.
+  x1 = cumsum(randn(1000, 5), 1);
+  x2 = cumsum(randn(1000, 5), 1);
+  x3 = cumsum(randn(1000, 5), 1);
+  trajectory_plotter('trajectories_test', 10, x1, x2, x3);
+
+ 
+
+%%
+vidObj = VideoWriter('practice.avi');
+open(vidObj);
+thetas = 1:1:360;
+for i = 1:length(thetas)
+    view([cosd(-135+thetas(i)) sind(-135+thetas(i)) 0.2])
+    pause(0.01)
+    thisFrame = getframe(gca);
+    thisFrame.cdata = thisFrame.cdata(1:1127,1:1127,:);
+    writeVideo(vidObj, thisFrame);
+end
+close(vidObj);
+
+figure('Position', [2000 0 1300 1300])
+axes('Units', 'Pixels', 'Position', [1 1 1300 1300], 'Color', [0 0 0])
+hold on
+axis(110*[-1 1 -1 1])
+cols = [];
+for i=1:7
+    cols = [cols; linspace(colors(i,1),0,30)' linspace(colors(i,2),0,30)' linspace(colors(i,3),0,30)'];
+end
+colormap(cols)
+
+
+
+
+
 
 %% Optional: estimating "signal variance"
 
