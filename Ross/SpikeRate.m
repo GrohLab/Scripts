2970f3d078f2f889c79eaae9120f1d8e58fa908b
Alexbmerkel <50135047+Alexbmerkel@users.noreply.github.com>
2020-03-18 22:36:13 +0100

    Create SpikeRate.m

---
+++ Ross/SpikeRate.m
@@ -0,0 +1,73 @@
+
+% This script gives spike rate for user-specified bin width for a 1 hr
+% recording for all units. 
+
+% We need the bin size.
+promptStrings = {'Bin size [s]:'};
+defaultInputs = {'0.05',};
+answ = inputdlg(promptStrings,'Inputs', [1, 30],defaultInputs);
+binSz = str2double(answ(1));
+
+   
+
+% We also need the sampling frequency, which can change from recording
+% to recording.
+promptStrings = {'Sampling Frequency (fs)'};
+defaultInputs = {'30000',};
+answ = inputdlg(promptStrings,'Inputs', [1, 30],defaultInputs);
+fs = str2double(answ(1));
+
+% This spiketrain variable is just dummy data.
+spiketrain = rand(fs*3600, 1);
+spiketrainlogical = spiketrain > 0.5;
+
+    
+% binSamples is the number of elements in spiketrain per bin.
+binSamples = fs*binSz;
+    
+% Now we need the no. of bins for the split up the histogram,
+% which is the length of the spiketrain vector divided by the binSamples. 
+nBins = round(size(spiketrainlogical, 1)/binSamples);
+% The reason we round is that nBins may not be an integer, which it
+% needs to be for the for-loop.
+% This will mean that the last bin for each recording may be a bit
+% off....(I can live with that...)
+
+
+% We now need to create a vector of size nBins to sum our samples into,
+% and we need to sum our samples bin by bin.
+
+%
+sizeT = size(spiketrain);
+counts = zeros(nBins,sizeT(1,2));
+% This column vector is the length of the no. of bins. 
+
+% We're going to replace each element 1 by 1 for the sum of the elements of spike train for the given bin size. 
+% We're going to go cluster by cluster, each cluster being a new column.
+
+for b = 1:sizeT(1,2);
+    
+    for a = 1:nBins
+        counts(a,b) = sum(spiketrainlogical(1 + binSamples*(a-1): a*binSamples, 1));
+    end
+    rate = counts/binSz;
+    figure; bar(rate);
+end
+% Rate will be given in Counts per Second.
+% If you had 20 counts in a half-second bin size, the rate would be 40
+% counts = 20/ bin size.
+% So you're dividing counts by the bin size.
+
+% We've churned out the firing rate per cluster fo a given bin width.
+
+
+% We also may want the counts for each cluster for a given bin width as a
+% matrix.
+countsfig = figure; bar(counts);
+
+% Whilst we're here, we may also want the binned firing rates for each
+% cluster as a matrix.
+
+rate = counts/binSz;
+ratefig = figure; bar(rate);
+
