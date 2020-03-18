f1baa69a8e9d796035a039c13091832003007de7
Alexbmerkel <50135047+Alexbmerkel@users.noreply.github.com>
2020-03-18 22:53:30 +0100

    Update SpikeRate.m

--- Ross/SpikeRate.m
+++ Ross/SpikeRate.m
@@ -18,7 +18,7 @@ answ = inputdlg(promptStrings,'Inputs', [1, 30],defaultInputs);
 fs = str2double(answ(1));
 
 % This spiketrain variable is just dummy data.
-spiketrain = rand(fs*3600, 1);
+spiketrain = rand(fs*3600, 3);
 spiketrainlogical = spiketrain > 0.5;
 
     
@@ -45,13 +45,13 @@ counts = zeros(nBins,sizeT(1,2));
 % We're going to replace each element 1 by 1 for the sum of the elements of spike train for the given bin size. 
 % We're going to go cluster by cluster, each cluster being a new column.
 
-for b = 1:sizeT(1,2);
+for b = 1:sizeT(1,2)
     
     for a = 1:nBins
         counts(a,b) = sum(spiketrainlogical(1 + binSamples*(a-1): a*binSamples, 1));
     end
     rate = counts/binSz;
-    figure; bar(rate);
+    figure; bar(rate(:,b));
 end
 % Rate will be given in Counts per Second.
 % If you had 20 counts in a half-second bin size, the rate would be 40
@@ -63,11 +63,13 @@ end
 
 % We also may want the counts for each cluster for a given bin width as a
 % matrix.
-countsfig = figure; bar(counts);
+% countsfig = figure; bar(counts);
 
 % Whilst we're here, we may also want the binned firing rates for each
 % cluster as a matrix.
 
-rate = counts/binSz;
-ratefig = figure; bar(rate);
+%rate = counts/binSz;
+%ratefig = figure; bar(rate);
+
+
 
