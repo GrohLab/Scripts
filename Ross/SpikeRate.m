b3d23c9ff1feca45b90631d45f68c510b89e792e
Alexbmerkel <50135047+Alexbmerkel@users.noreply.github.com>
2020-03-19 13:23:40 +0100

    Update SpikeRate.m

--- Ross/SpikeRate.m
+++ Ross/SpikeRate.m
@@ -60,10 +60,10 @@ end
 
 % We've churned out the firing rate per cluster fo a given bin width.
 % Lets plot them next to eachother to get population activity.
-% Experiment time in msecs for the RateMap x-axis
+% Experiment time in msecs for the RateMap x-axis.
 exptT = [0:nBins]'*(binSz/0.001);
-
-imagesc(exptT, [1:szT], rate');
-
+ 
+figure; imagesc(exptT, [1:szT], rate');
+% x-axis = exptT, y-axis = [1:szT], plot = rate'.
 
 
