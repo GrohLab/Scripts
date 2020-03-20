6dde7f71b5e93dee74349ec7f52ada16ba246a41
Alexbmerkel <50135047+Alexbmerkel@users.noreply.github.com>
2020-03-20 16:46:09 +0100

    Update SpikeRate2.m

--- Ross/SpikeRate2.m
+++ Ross/SpikeRate2.m
@@ -12,7 +12,7 @@ binSz = str2double(answ(1));
 % We also need the sampling frequency, which can change from recording
 % to recording.
 promptStrings = {'Sampling Frequency (fs)'};
-defaultInputs = {'30000',};
+defaultInputs = {'3.003003003003003e+04',};
 answ = inputdlg(promptStrings,'Inputs', [1, 30],defaultInputs);
 fs = str2double(answ(1));
 
@@ -27,18 +27,20 @@ fs = str2double(answ(1));
 % binSamples is the number of elements in spiketrain per bin.
 binSamples = fs*binSz;
 
-% Need the number of bins we're gonna pop our data into
+% Need the number of bins we're gonna pop our data into.
 nBins = round(head68.npoints/binSamples);
 
 
 % Need to create an empty matrix that's eagerly awaiting all our cluster
-% counts
-szT = length(sortedData);
+% counts.
+badsIdx = cellfun(@(x) x==3,sortedData(:,3));
+nBads = sum(badsIdx);
+% Need to know the number of bad clusters that we not going to bother with.
+szT = length(sortedData)- nBads;
 counts = zeros(nBins, szT);
 
 for b = 1:szT   % cluster by cluster
     if sortedData{b,3} == 3 % don't want to waste time on bad clusters
-        counts(:,b) = [];
     else
         for a = 1:nBins
             fsV = fs*sortedData{b,2}'; 
@@ -46,11 +48,11 @@ for b = 1:szT   % cluster by cluster
             logicalfsV = (fsV >(a-1)*binSamples) & (fsV <= a*binSamples);
             counts(a,b) = sum(logicalfsV);
         end
-        rate = counts/binSz;
-        figure; bar(rate(:,b));
+        % rate = counts/binSz;
+        % figure; bar(rate(:,b));
     end
 end
-% rate = counts/binSz;
+ rate = counts/binSz;
 % Rate will be given in Counts per Second.
 % If you had 20 counts in a half-second bin size, the rate would be 40
 % counts = 20/ bin size.
