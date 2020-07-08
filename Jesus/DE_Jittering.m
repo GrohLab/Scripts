6f08e4e6a421db8cd8900eaeb280ec31e4b9c44b
Emilio Isaías-Camacho <isaias@ana.uni-heidelberg.de>
2020-07-08 18:21:29 +0200

    Output the directionality in the results

--- Jesus/DE_Jittering.m
+++ Jesus/DE_Jittering.m
@@ -243,7 +243,8 @@ delta_t = diff(responseWindow);
 indCondSubs = cumsum(Nccond:-1:1);
 consCondNames = condNames(consideredConditions);
 % Plotting statistical tests
-figs = scatterSignificance(Results, Counts, consCondNames, delta_t, sortedData(goods,1));
+[figs, Results] = scatterSignificance(Results, Counts,...
+    consCondNames, delta_t, sortedData(goods,1));
 configureFigureToPDF(figs);
 stFigBasename = fullfile(figureDir,[expName,' ']);
 stFigSubfix = sprintf(' Stat RW%.1f-%.1fms SW%.1f-%.1fms',...
