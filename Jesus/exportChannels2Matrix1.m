784831d36096feb69ad08424dc8feac40c378e71
jesusmartin-cortecero <53047456+jesusmartin-cortecero@users.noreply.github.com>
2020-08-13 19:33:15 +0200

    Spike2tomat--to2D--Matrix

---
+++ Jesus/exportChannels2Matrix1.m
@@ -0,0 +1,36 @@
+
+clear all
+%open('D:\mat to brainstorm\TestAllChannels1.mat')
+path='D:\mat to brainstorm\'
+myfile='TestAllChannels1.mat'
+cd(path)
+names=fieldnames(open(myfile));
+goods=[];
+for i=1:numel(names)
+    if ~isempty(strfind(names{i},'Ch'))
+        if isempty(strfind(names{i},'Ch31'))
+            goods=[goods;i];
+        end
+    end
+end
+names=names(goods)
+
+%get number sample points
+firstchan=load(myfile,names{1})
+eval(['nsamples=firstchan.' names{1} '.length;'])
+nchan=numel(names);
+%%
+V=zeros(nsamples,nchan);  %%preallocate space
+
+%get every individual channel
+for i=1:nchan
+    thischan=names{i}; %name of channel as string
+    v=load(myfile,thischan); %load channel from file
+    eval([' V(:,i)=v.' thischan '.values;']) %put channel values in to preallocated matrix
+    clear v
+   
+end
+
+outputfile=[myfile(1:end-4) '_matrix']
+save(outputfile,'V')
+save(outputfile,'V','-v7.3')
