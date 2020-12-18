ecdbb1546e4432cdefb4772a1fb89a6b27ff9be3
Emilio Isaías-Camacho <isaias-camacho@physiologie.uni-heidelberg.de>
2020-12-18 18:25:56 +0100

    Placed real names for the methods

--- protocolGetterScript.m
+++ protocolGetterScript.m
@@ -2,7 +2,7 @@ dataDir = 'Path\to\your\data';
 pgObj = ProtocolGetter(dataDir);
 pgObj.getConditionSignals;
 pgObj.getSignalEdges;
-pgObj.getFrequencySubs;
+pgObj.getFrequencyEdges;
 pgObj.pairStimulus;
 % In between, you should verify the Conditions property of the pgObj
 % variable if it matches the real stimulation protocol. If it mathces, then
