dd846f23914b22f97f059b24e60f9dc0a6bd3c59
Alexbmerkel <50135047+Alexbmerkel@users.noreply.github.com>
2021-03-05 09:24:02 +0100

    plots laser pulse width

--- Ross/plotDepthVsLatencies.m
+++ Ross/plotDepthVsLatencies.m
@@ -1,5 +1,5 @@
 function fig = plotDepthVsLatencies(clInfo, clIDs, ConditionName, TriggerTimes, sortedData, samplingFrequency)
-
+fs = samplingFrequency;
 clInd = ismember(clInfo.id, clIDs);
 depths = table(clInfo.id(clInd), clInfo.AbsDepth(clInd));
 depths = sortrows(depths,'Var2','ascend');
@@ -10,41 +10,58 @@ for i = 1:length(ID)
     spkInd = [spkInd; find(ismember(sortedData(:,1), ID(i)))];
 end
 name = ConditionName;
-% name(strfind(name, '_')) = ' ';
-name = ['Depth vs Latency ', name];
-Latencies = TriggerLatencies(sortedData(spkInd,2), TriggerTimes, samplingFrequency);
+name(strfind(name, '_')) = ' ';
+name = ['Depth vs Latency: ', name];
+Latencies = TriggerLatencies(sortedData(spkInd,2), TriggerTimes, fs);
 mn = (cellfun(@mean, Latencies)*1e3);
 sd = (cellfun(@std, Latencies)*1e3);
 Dpth = -1*Dpth;
 fig = figure('Name', name, 'Color', 'White');
 
-errorbar(mn,Dpth,sd, 'horizontal', 'LineStyle', 'none', 'Marker', 'd', 'Color', 'k', 'MarkerSize', 2.5);
+errorbar(mn,Dpth,sd, 'horizontal', 'LineStyle', 'none', 'Marker', 'd', 'Color', [0.5, 0.5, 0.5], 'MarkerSize', 2.5, 'LineWidth', 0.01, 'CapSize', 5);
+
+%Working on shit
+% ind = find(isnan(mn));
+% mn(ind) = [];
+% sd(ind) = [];
+% Dpth(ind) = [];
+% sd(sd == false) = 0.1;
+% hold on
+% for i = 1:length(mn)
+%     x1 = mn(i)-sd(i);
+%     x2 = mn(i) + sd(i);
+%     plot(x1,Dpth(i), 'LineStyle', 'none', 'Marker', '<', 'Color', [0.5, 0.5, 0.5], 'MarkerSize', sd(i))
+%     plot(x2,Dpth(i), 'LineStyle', 'none', 'Marker', '>', 'Color', [0.5, 0.5, 0.5], 'MarkerSize', sd(i))
+% end
+%     
 
 
-
-
-%text(median(Latencies{i}),Dpth(i),[' ', ID{i}], 'FontSize', 8);
+pulseWidth = round(median(diff(TriggerTimes'/fs)')*1e3);
 title(name, 'Interpreter', 'none');
 ylim([round(round(min(Dpth),-2)/2,-2)*2-200,0]);
 yticks([round(round(min(Dpth),-2)/2,-2)*2-200:200:0]);
 xlim([0 50]);
 xticks(0:5:50);
 xlabel('Latency_{(ms)}');
-ylabel('Depth_{(um)}')
+ylabel('Depth_{(\mum)}', 'Interpreter', 'tex')
 % ax.XTickLabelRotation = -45;
 ax = gca;
 hold on
-plot([0:10], (ax.YLim(1)+25)*ones(11,1), 'Color', [0 1 1 0.5], 'LineWidth', 3)
+laser = plot([0:pulseWidth], (ax.YLim(1)+25)*ones(pulseWidth + 1,1), 'Color', [0 1 1 0.5], 'LineWidth', 3);
 ax = gca;
 ax.FontSize = 20;
 % ax.FontName = 'Times New Roman';
 % Create rectangle
-legend({'mean +/- SD'});
+lsText = text(3, ax.YLim(1)+45, 'Laser Pulse', 'FontSize', 10);
+
+lgd = legend;
+lgd.String{1} = 'Unit Mean +/- SD';
+lgd.String{2} = ' Laser Pulse';
 
 % rectangle(ax, 'Position', [1, ax.YLim(2)-100, 4, 50], ...
 % 'LineStyle', 'none', 'FaceColor', [0 1 1 0.5])
-text(3, ax.YLim(1)+45, 'Laser Pulse', 'FontSize', 10);
-rectangle(ax, 'Position', [ax.XLim(1), ax.YLim(1), 10, ax.YLim(2) - ax.YLim(1)], ...
+
+rectangle(ax, 'Position', [ax.XLim(1), ax.YLim(1), pulseWidth, ax.YLim(2) - ax.YLim(1)], ...
 'LineStyle', 'none', 'FaceColor', [0 1 1 0.1])
 end
 
