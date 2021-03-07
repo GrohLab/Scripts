de5e7c577ecbc9381b014c729d045600d9ebaeb6
Alexbmerkel <50135047+Alexbmerkel@users.noreply.github.com>
2021-03-07 14:37:31 +0100

    Create plotLatencyVsTriggeredRates

---
+++ Ross/plotLatencyVsTriggeredRates.m
@@ -0,0 +1,71 @@
+function fig = plotLatencyVsTriggeredRates(latencyCutoffs, sdCutoffs, clInfo, clIDs, ConditionName, TriggerTimes, sortedData, samplingFrequency)
+fs = samplingFrequency;
+clInd = ismember(clInfo.id, clIDs);
+depths = table(clInfo.id(clInd), clInfo.AbsDepth(clInd));
+depths = sortrows(depths,'Var2','ascend');
+ID = depths{:,1};
+Dpth = depths.Var2;
+spkInd = [];
+for i = 1:length(ID)
+    spkInd = [spkInd; find(ismember(sortedData(:,1), ID(i)))];
+end
+name = ConditionName;
+name(strfind(name, '_')) = ' ';
+name = ['Latency vs Triggered Rate: ', name];
+Latencies = TriggerLatencies(sortedData(spkInd,2), TriggerTimes, fs, 5e-2);
+Rates = TriggeredRates(sortedData(spkInd,2), TriggerTimes, fs, 5e-2);
+mnLat = (cellfun(@mean, Latencies)*1e3);
+sdLat = (cellfun(@std, Latencies)*1e3);
+mnRate = cellfun(@mean, Rates);
+sdRate = cellfun(@std, Rates);
+latencyCutoffs = sort(latencyCutoffs, 'ascend');
+sdCutoffs = sort(sdCutoffs, 'ascend');
+
+tagged = latencyCutoffs(1) <= mnLat & mnLat <= latencyCutoffs(2) & sdCutoffs(1) <= sdLat & sdLat <= sdCutoffs(2);
+taggedInd = spkInd(tagged);
+TaggedIDs = sortedData(taggedInd,1);
+
+fig = figure('Name', name, 'Color', 'White');
+
+
+errorbar(mnLat(~tagged),mnRate(~tagged),sdRate(~tagged), sdRate(~tagged), sdLat(~tagged), sdLat(~tagged),...
+    'LineStyle', 'none', 'Marker', 'd', 'Color', [0.5, 0.5, 0.5], 'MarkerSize', 2.5, 'LineWidth', 0.01, 'CapSize', 5);
+hold on
+errorbar(mnLat(tagged),mnRate(tagged),sdRate(tagged), sdRate(tagged),  sdLat(tagged), sdLat(tagged),...
+    'LineStyle', 'none', 'Marker', 'd', 'Color', [0.5, 1, 1], 'MarkerSize', 2.5, 'LineWidth', 0.01, 'CapSize', 5);
+hold off
+
+
+pulseWidth = round(median(diff(TriggerTimes'/fs)')*1e3);
+% title(name, 'Interpreter', 'none');
+ylim([0, round(max(mnRate)+max(sdRate),-1)]);
+% yticks([round(round(min(Dpth),-2)/2,-2)*2-200:200:0]);
+xlim([0 50]);
+xticks(0:5:50);
+xlabel('Latency_{(ms)}');
+ylabel('Firing Rate_{(Hz)}')
+% ax.XTickLabelRotation = -45;
+ax = gca;
+hold on
+laser = plot([0:pulseWidth], ax.YLim(2)*ones(pulseWidth + 1,1), 'Color', [0 1 1 0.5], 'LineWidth', 3);
+ax = gca;
+ax.FontSize = 20;
+% ax.FontName = 'Times New Roman';
+% Create rectangle
+% lsText = text(3, ax.YLim(1)+45, 'Laser Pulse', 'FontSize', 10);
+
+lgd = legend;
+lgd.String{1} = (['Unit Mean +/- SD (n=', num2str(sum(~tagged)), ')']);
+lgd.String{2} = (['Opto-tagged Units (n=', num2str(sum(tagged)), ')']);
+lgd.String{3} = ['Laser Pulse (',num2str(pulseWidth), 'ms)'];
+% rectangle(ax, 'Position', [1, ax.YLim(2)-100, 4, 50], ...
+% 'LineStyle', 'none', 'FaceColor', [0 1 1 0.5])
+
+rectangle(ax, 'Position', [ax.XLim(1), ax.YLim(1), pulseWidth, ax.YLim(2) - ax.YLim(1)], ...
+'LineStyle', 'none', 'FaceColor', [0 1 1 0.1])
+lgd.FontSize = 8;
+
+
+end
+
+
