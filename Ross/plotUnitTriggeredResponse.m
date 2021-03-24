2b7ee5ce37722762a04834c85925469d96810065
Alexbmerkel <rossfolkard@gmail.com>
2021-03-24 15:45:13 +0100

    Update plotUnitTriggeredResponse.m

--- Ross/plotUnitTriggeredResponse.m
+++ Ross/plotUnitTriggeredResponse.m
@@ -59,6 +59,13 @@ Na = sum(delayFlags,1);
 trigSubset = sort(randsample(Na,Na));
 tLoc = find(delayFlags);
 tSubs = tLoc(trigSubset);
+% Trigger subset for stimulation shading
+trigAlSubs = CondTriggers(trigSubset,:);
+timeDur = round(diff(trigAlSubs, 1, 2)/fs, 3);
+trigChange = find(diff(timeDur) ~= 0);
+
+
+
 binSz = 0.1;
 if class(ID) == 'cell'
     figureName = ID{1};
@@ -66,11 +73,26 @@ else
     figureName = ID;
 end
 % figure('Name', ['Unit ', figureName], 'Color', 'White');
+
+subplot(2,1,1)
 ax = gca;
 plotRasterFromStack(discStack([1,2],:,tSubs),...
     timeLapse, fs,figureName, ax);
-ax1 = gca;
 hold on
+% initSub = 0;
+% optsRect = {'EdgeColor',clr,'FaceColor','none'};
+% for ctr = 1:numel(trigChange)
+%     rectangle('Position',[0, initSub,...
+%         timeDur(trigChange(ctr)), trigChange(ctr)],optsRect{:})
+%     initSub = trigChange(ctr);
+% end
+% rectangle('Position', [0, initSub, timeDur(Na),...
+%     Na - initSub],optsRect{:})
+
+
+% PSTH
+
+
 
 
 [PSTH, trig, sweeps] = getPSTH(discStack([1,2],:,:),timeLapse,...
@@ -82,17 +104,20 @@ tSubs = tObj.subTriggers;
 if ~isempty(tSubs)
     for ct = 1:size(tSubs,1)
         for nf = 1:size(tSubs,2)
-            line(ax1,'XData',repmat(trigTX(tSubs(ct,nf)),1,2),'YData',[0.5,Na+0.5],...
+            line(ax,'XData',repmat(trigTX(tSubs(ct,nf)),1,2),'YData',[0.5,Na+0.5],...
                 'Color',clr,'LineWidth',2)
         end
     end
-else
+% else
     trig = logical(trig/max(trig(:)));
-    line(ax1,'XData',trigTX,'YData',(Na+1.5)*trig - 0.5,...
-       'Color',clr,'LineWidth',2)
+    line(ax,'XData',trigTX,'YData',(Na+1.5)*trig - 0.5,...
+        'Color',clr,'LineWidth',2)
 end
+ax.YAxis.Label.String = ['# Trials'];
+ax.FontSize = 20;
+ax.FontName = 'Arial';
 
-yyaxis('right')
+subplot(2,1,2)
 
 
 
@@ -107,16 +132,59 @@ maxYLabel = round(cnv + 5, -1);
 mxYTick = ax.YLim(2)*maxYLabel/cnv;
 ax.YLim(2) = mxYTick;
 yscl = [ax.YLim(1):10/sc: ax.YLim(2)];
-ax.YAxis(2).TickValues = yscl;
-ax.YAxis(2).TickLabels = sc.*ax.YAxis(2).TickValues;
+ax.YAxis.TickValues = yscl;
+ax.YAxis.TickLabels = sc.*ax.YAxis.TickValues;
 
+line('XData',trigTX,'YData',(0.25*mxYTick)*trig - 0.5,...
+    'Color',clr,'LineWidth',2);
 
-ax.YAxis(2).Color = [1,0.4,0.4];
-ax.YAxis(2).Label.String = ['Firing Rate _{(Hz)}'];
-ax.YAxis(1).Label.String = ['# Trials'];
+ax.YAxis.Label.String = ['Firing Rate _{(Hz)}'];
 ax.XLabel.String = 'Time _{(s)}';
 ax.FontSize = 20;
 ax.FontName = 'Arial';
-end
+
+
+
+
+
+
+% stims = mean(cst(:,:,delayFlags,3));
+%     stims = stims - median(stims,2);
+%     for cs = 1:size(stims,1)
+%         if abs(log10(var(stims(cs,:),[],2))) < 13
+%             [m,b] = lineariz(stims(cs,:),1,0);
+%             stims(cs,:) = m*stims(cs,:) + b;
+%         else
+%             stims(cs,:) = zeros(1,Nt);
+%         end
+%     end
+% ax3 = subplot(totlX,1,5,'Parent',fig);
+% [r,c] = size(stims);
+% if r < c
+%     stims = stims';
+% end
+% 
+% for cs = 1:min(r,c)
+%     if exist('IDs','var')
+%         plot(ax3,trigTX,stims(:,cs),'LineStyle','-.','LineWidth',0.5,...
+%             'DisplayName', IDs{cs})
+%     else
+%         plot(ax3,trigTX,stims(:,cs),'LineStyle','-.','LineWidth',0.5)
+%     end
+%     
+%     ax3.Children(1).Color = defineColorForStimuli(IDs(cs));
+%     
+%     if cs == 1
+%         ax3.NextPlot = 'add';
+%     end
+% end
+% legend(ax3,'show','Location','best')
+% 
+% ax3.Box = 'off';
+% ax3.XLim = [timeLapse(1), timeLapse(2)];
+% ax3.XAxis.Visible = 'off';
+% ax3.YAxis.Visible = 'off';
+% linkaxes([ax1,ax2,ax3],'x')
+% end
 
 
