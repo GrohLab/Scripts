46a95a75e60a6cdad7968c43b44b33d91ac5b683
Alexbmerkel <rossfolkard@gmail.com>
2021-03-25 12:44:32 +0100

    Correcting condIndices & whatnot

--- Ross/plotTriggeredDoseResponse.m
+++ Ross/plotTriggeredDoseResponse.m
@@ -25,11 +25,11 @@ for b = 1:lngth
         rate = TriggeredRates(train, Cond{a},fs, timeBin);
         rates(b,a) = mean(rate{:});
         sds(b,a) = std(rate{:});
-        if contains(Conditions(condIndices(a)).name, '1Hz') % Can make it so we don't need to know text i.e. Hz Ind = yadayadayada...later
+        if contains(Conditions(condIndices(a)).name, "1Hz") % Can make it so we don't need to know text i.e. Hz Ind = yadayadayada...later
             conds(a) = 1;
-        elseif contains(Conditions(condIndices(a)).name, '10Hz')
+        elseif contains(Conditions(condIndices(a)).name, "10Hz")
             conds(a) = 2;
-        elseif contains(Conditions(condIndices(a)).name, 'pulse')
+        elseif contains(Conditions(condIndices(a)).name, ["pulse", "control"], 'IgnoreCase', true)
             conds(a) = 3;
         end
     end
@@ -38,6 +38,11 @@ end
 cond1 = conds == 1;
 cond2 = conds == 2;
 cond3 = conds == 3;
+condFlg = [cond1; cond2; cond3];
+consConds = sum(condFlg,2) ~= false;
+
+
+condInd = find(consConds);
 legText = [];
 legendCell = [{['1Hz', legText]}, {['10Hz', legText]}, {['Pulse', legText]}];
 ax = gca;
@@ -47,17 +52,22 @@ ax = gca;
 xnames = cell(1, length(condIndices));
 pwrs = zeros(1, length(condIndices));
 for a = 1:length(condIndices)
+    mWind = strfind(Conditions(condIndices(a)).name, 'mW');
     undInds = strfind(Conditions(condIndices(a)).name, '_');
+    mwUnd = undInds(undInds<mWind);
+    mWundInd = mwUnd(end);
+    pwrs(a) = str2double(Conditions(condIndices(a)).name(mWundInd+1:mWind-1));
+    
     Conditions(condIndices(a)).name(undInds) = ' ';
     Conditions(condIndices(a)).name = Conditions(condIndices(a)).name(undInds(1)+1:undInds(end)-1);
-    if strfind(Conditions(condIndices(a)).name, '5sec')
+    if strfind(Conditions(condIndices(a)).name, 'sec')
         Conditions(condIndices(a)).name = Conditions(condIndices(a)).name(6:end);
         Conditions(condIndices(a)).name(1) = 'P';
     end
     xnames{a} = Conditions(condIndices(a)).name;
     spaceInd = strfind(xnames{a}, ' ');
-    mWind = strfind(xnames{a}, 'mW');
-    pwrs(a) = str2double(xnames{a}(spaceInd+1:mWind-1));
+    
+    
 end
 pwrs = unique(pwrs);
 for a = 1:length(pwrs)
@@ -67,16 +77,17 @@ end
 hold on
 x = 1:length(pwrs);
 offset = 0.1;
-errorbar(ax, x-offset, rates(cond1), sds(cond1), 'vertical', 'LineWidth', 3)
-errorbar(ax, x, rates(cond2), sds(cond2), 'vertical', 'LineWidth', 3)
-errorbar(ax, x+offset, rates(cond3), sds(cond3), 'vertical', 'LineWidth', 3)
+for a = 1:length(condInd)
+    errorbar(ax, x + offset*(a-1), rates(condFlg(condInd(a),:)), sds(condFlg(condInd(a),:)), 'vertical', 'LineWidth', 3);
+end
 
-legend(legendCell, 'Location', 'northwest')
+legend(legendCell(condInd), 'Location', 'northwest')
 xlabel('Laser Intensity _{(mW)}');
 ax.XLim(1) = 0;
 ax.XLim(2) = length(pwrs) + 1;
 ax.XTick = [1:1:length(pwrs)];
 ax.XTickLabel = pwrCell;
+ax.YLim(1) = 0;
 ylabel('Firing Rate _{(Hz)}');
 ax.FontSize = 20;
 ax.FontName = 'Arial';
