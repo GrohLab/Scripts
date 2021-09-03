ab8d289bfbc2bcc2b9f67ebefed0da008c1fb376
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2021-09-03 18:59:59 +0200

    Update RollerCorrection.m

--- Emilio/RollerCorrection.m
+++ Emilio/RollerCorrection.m
@@ -5,5 +5,6 @@ camp = arrayfun(@(x) interp1(stTx, vStack(:,x), cpts{x,1}),...
     (1:size(vStack,2))', "UniformOutput", false);
 [~, mxSub] = cellfun(@max, camp, 'UniformOutput', false);
 dlTms = arrayfun(@(x) cpts{x,1}(mxSub{x}), (1:size(cpts,1))',"UniformOutput",false);
-ttms = sort(cat(1,Conditions([3:5]).Triggers))./fs;
+ttms = sort(cat(1,Conditions(3:5).Triggers))./fs;
+emptyCells = cellfun(@isempty, dlTms);
 pts = [cat(1,dlTms{~emptyCells}), ttms(~emptyCells)];
