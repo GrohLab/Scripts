9757c00f4a3e983391badf4eab7d79b11a6fcab4
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2021-09-03 17:47:35 +0200

    Re-organised the folder

---
+++ Emilio/RollerCorrection.m
@@ -0,0 +1,9 @@
+fnOpts = {"UniformOutput", false};
+cpts = getWaveformCriticalPoints(vStack, fsRoll);
+cpts = cellfun(@(x) x - 1, cpts, 'UniformOutput', false);
+camp = arrayfun(@(x) interp1(stTx, vStack(:,x), cpts{x,1}),...
+    (1:size(vStack,2))', "UniformOutput", false);
+[~, mxSub] = cellfun(@max, camp, 'UniformOutput', false);
+dlTms = arrayfun(@(x) cpts{x,1}(mxSub{x}), (1:size(cpts,1))',"UniformOutput",false);
+ttms = sort(cat(1,Conditions([3:5]).Triggers))./fs;
+pts = [cat(1,dlTms{~emptyCells}), ttms(~emptyCells)];
