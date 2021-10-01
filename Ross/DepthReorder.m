82c1018666302595010f1cb0f3df4fd022acc0a8
Ross Folkard <rossfolkard@gmail.com>
2021-10-01 17:13:15 +0200

    Update DepthReorder.m

--- Ross/DepthReorder.m
+++ Ross/DepthReorder.m
@@ -1,4 +1,6 @@
 % Unit Depth Re-ordering besed on corrected channel maps.
+load('Z:\Emilio\Cambridge_NeuroTech_H3.mat')
+load('Z:\Ross\ProbeFiles\Adaptor-Amplifier_Mapping.mat')
 depths = [0:20:1275]';
 for ch = 1:length(chanMap0ind)
     chNo = chanMap0ind(ch);
