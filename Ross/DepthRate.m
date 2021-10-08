a3a61085f6fbf6d99f7309a9bce39066da4d1802
Ross Folkard <rossfolkard@gmail.com>
2021-10-08 15:50:34 +0200

    Create DepthRate.m
    Takes Changes in Rates (Mech, Mech + Laser) and plots them along with the laser modulation of Mech FRs

---
+++ Ross/DepthRate.m
@@ -0,0 +1,76 @@
+%% Prep Variables
+mrControl = clInfo.Mech_Control_4mW_Rate_Evoked-clInfo.Mech_Control_4mW_Rate_Spont;
+mrLaser = clInfo.Mech_Laser_5sec_4mW_Rate_Evoked-clInfo.Mech_Laser_5sec_4mW_Rate_Spont;
+data = table(clInfo.id, clInfo.abs_depth, mrControl, mrLaser);
+mrInd = ismember(data.Var1, clInfo.id(clInfo.Mech_Control_4mW_R==true));
+data = data(find(mrInd),:);
+data = sortrows(data,'Var2','ascend');
+ID = data.Var1;
+depths = data.Var2;
+c = data.Var3;
+l = data.Var4;
+jitter = randi(20,size(depths));
+depths = -1*(depths + jitter);
+
+plottedData = [{'Mech'}, {[zeros(length(c),1), c]};...
+    {'Mech + L6'}, {[zeros(length(l),1), l]};...
+    {'L6 Modulation of Mechanical Responses'}, {[zeros(length(l),1), l-c]}];
+
+
+%% Plotting
+figure('Color', 'White');
+firstLater = find(ismember(ID,Later), 1, 'first');
+firstLatest = find(ismember(ID,Latest), 1, 'first');
+firstL6 = find(ismember(ID,L6), 1, 'first');
+for fg = 1:3
+    subplot(2,2,fg);
+    if fg == 3
+        hold on
+        subplot(2,2,3:4)
+    end
+    hold on
+    
+    % Plotting the first of each group for the legend
+    dr = plottedData{fg,2}(firstLater,:);
+    dpth = [depths(firstLater), depths(firstLater)];
+    plot(dr, dpth, 'LineStyle', '-', 'Color', [1, 0, 0, 0.5], 'LineWidth', 10);
+    
+    dr = plottedData{fg,2}(firstLatest,:);
+    dpth = [depths(firstLatest), depths(firstLatest)];
+    plot(dr, dpth, 'LineStyle', '-', 'Color', [0, 1, 0, 0.5], 'LineWidth', 10);
+    
+    dr = plottedData{fg,2}(firstL6,:);
+    dpth = [depths(firstL6), depths(firstL6)];
+    plot(dr, dpth, 'LineStyle', '-', 'Color', [0, 0.5, 1, 0.5], 'LineWidth', 10);
+    leg = legend;
+    leg.String = [{'Group 1'}, {'Group 2'}, {'L6'}];
+    
+    
+    
+    
+    
+    
+    for unit = 1:length(ID)
+        dr = plottedData{fg,2}(unit,:);
+        dpth = [depths(unit), depths(unit)];
+        if ismember(ID{unit}, L6) & unit ~= firstL6
+            plot(dr, dpth, 'LineStyle', '-', 'Color', [0, 0.5, 1, 0.5], 'LineWidth', 10);
+        elseif ismember(ID{unit}, Later) & unit ~= firstLater
+            plot(dr, dpth, 'LineStyle', '-', 'Color', [1, 0, 0, 0.5], 'LineWidth', 10);
+        elseif ismember(ID{unit}, Latest) & unit ~= firstLatest
+            plot(dr, dpth, 'LineStyle', '-', 'Color', [0, 1, 0, 0.5], 'LineWidth', 10);
+        end
+    end
+    ax = gca;
+    ax.Title.String = plottedData{fg,1};
+    ax.YLabel.String = 'Depth [\mum]';
+    ax.XLabel.String = '\DeltaRate [Hz]';
+    ax.FontName = 'Arial';
+    ax.FontSize = 20;
+    ax.YLim = [-1600, 0];
+    % ax.XLim = [-50, 50];
+    % ax.XTick = [-50:10:50];
+    plot(zeros(length(ID)), linspace(ax.YLim(1),ax.YLim(2), length(ID)), 'LineStyle', '-', 'Color', 'k');
+    leg.String(4:end) = [];
+    leg.String(4:end) = [];
+end
