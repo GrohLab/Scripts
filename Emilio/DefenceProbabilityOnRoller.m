c1ff80a471c86a5c8853597850f9b962e585ff52
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2021-11-03 18:47:05 +0100

    Change in parameters for batch 2

--- Emilio/DefenceProbabilityOnRoller.m
+++ Emilio/DefenceProbabilityOnRoller.m
@@ -4,7 +4,7 @@ excludeFlag = rms(vStack(spontFlag,:)) > 0.85 | rms(vStack) > 0.9;
 % excludeFlag = rms(vStack(spontFlag,:)) < 0.85;
 Na = sum(delayFlags & ~excludeFlag(:));
 rngRollSpeed = cell(Nccond,1);
-responseWindow = [-0.2, 1];
+responseWindow = [0, 0.4];
 responseFlags = stTx >= responseWindow(1) & stTx <= responseWindow(2);
 for ccond = 1:Nccond
     rngRollSpeed{ccond} =...
@@ -17,7 +17,7 @@ speedsMat = cellfun(@(x) cat(1, x, nan(maxNt - size(x,1),1)),...
     rngRollSpeed, fnOpts{:}); speedsMat = cat(2, speedsMat{:});
 speedsMat = speedsMat * en2cm;
 % Cutting on different thresholds for all trials
-thetaSpeed = 0.2:0.2:2;
+thetaSpeed = 0.2:0.2:6;
 moveFlag = speedsMat > reshape(thetaSpeed,1,1,[]);
 probMove = sum(moveFlag)./Na; probMove = squeeze(probMove);
 probFig = figure; plot(thetaSpeed, probMove'); ylim([0,1])
@@ -33,7 +33,7 @@ if strcmpi(sveFigAns, "Yes")
     saveFigure(probFig, fullfile(figureDir, 'Movement probability'), 1, 1)
 end
 %% Bar plotting
-condSubs = [1,2]; thS = 8;
+condSubs = [1:7]; thS = 20;
 % Table with the successful trials in the first column.
 tbl = [probMove(condSubs, thS).*Na(condSubs)',...
     abs(probMove(condSubs, thS).*Na(condSubs)' - Na(condSubs)')];
@@ -41,11 +41,14 @@ tbl = [probMove(condSubs, thS).*Na(condSubs)',...
 sumCol = sum(tbl)'; sumFil = sum(tbl,2)';
 multSum = (sumCol * sumFil)'; esperado = multSum / sum(tbl(:));
 resChi = ((tbl - esperado).^2)./esperado; resChi = sum(resChi(:));
-pChi = chi2pdf(resChi,1); [~, pFih] = fishertest(tbl);
+pChi = chi2pdf(resChi,1); pFih = Inf;
+if all(size(tbl) == [2, 2])
+    [~, pFih] = fishertest(tbl);
+end
 % Plotting the proportions and test results
 barFig = figure; bar(probMove(:,thS), "EdgeColor", "none");
 yHeight = max(probMove(condSubs, thS))*1.05;
-hold on; plot(condSubs, yHeight * ones(2,1),...
+hold on; plot(condSubs, yHeight * ones(size(condSubs,2),1),...
     'LineWidth', 1, "Color", 'k')
 signStr = 'n.s.';
 if pChi < 5e-3 || pFih < 5e-3
