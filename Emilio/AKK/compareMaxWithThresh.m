2c1e9ce6c759f71fed38d755c6dd54f959c793ae
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2022-01-18 19:23:30 +0100

    Create compareMaxWithThresh.m

---
+++ Emilio/AKK/compareMaxWithThresh.m
@@ -0,0 +1,24 @@
+function [moveFlag] = compareMaxWithThresh(mx, thCell)
+%COMPAREMAXWITHTHRESH compares each column of the maximum value per trial
+%per signal against a given threshold set.
+%   Detailed explanation goes here, later
+%% 
+% Validation section for size and compatibility.
+% Number of signals
+Ns = size(mx, 2);
+
+% Number of given cells
+Nth = numel(thCell);
+
+if Ns ~= Nth
+    fprintf(1, "The signals in the stack is different from the number of")
+    fprintf(1, " sets given!\n")
+    fprintf(1, "Please, check your variables!\n")
+    moveFlag = {};
+    return
+end
+
+moveFlag = arrayfun(@(x) mx(:,x) < thCell{x}, 1:Ns,...
+    "UniformOutput", false);
+
+end
