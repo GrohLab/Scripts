b5d48c6b5875cdf1403106259b8558dad3166d8f
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2023-05-09 17:31:28 +0200

    More than one behRes for the session

--- Emilio/poolBehIndices.m
+++ Emilio/poolBehIndices.m
@@ -5,6 +5,7 @@ expandName = @(x) fullfile(x.folder, x.name);
 animalPattern = '[A-Za-z]+\d{2,}';
 rsOpts = {animalPattern, 'SearchType', 'expression'};
 ctOpts = {'IgnoreCase', true};
+lsOpts = {'L\d+.\d+', 'match'};
 %% Assuming 1 level of animal organisation i.e.
 % BatchX/FolderA/Animal001
 % BatchX/FolderB/Animal002
@@ -32,28 +33,53 @@ for cad = animalFolders(:)'
         'match')), sessDirs, fnOpts{:}); 
     sessDirs(cellfun(@isempty, onlyDateSessFlag)) = [];
     for csd = sessDirs(:)'
-        currSess = csd.name;
-        behFigDir = recursiveFolderSearch(expandName(csd),...
+        ephDir = expandName(dir(fullfile(expandName(csd), 'ephys*')));
+        if isempty(ephDir)
+            fprintf(1, "This session didn't have ephys!\n")
+            fprintf(1, "Skipping\n")
+            continue
+        end
+        currSess = char(regexp(csd.name, '\d{6}', 'match'));
+        behFigDir = recursiveFolderSearch(ephDir,...
             "Beh V-0.25 - 0.50 s R5.00 - 400.00 ms");
         behIdxFiles = dir(fullfile(behFigDir, "BehIndex*.fig"));
-        if numel(behIdxFiles) == 1
-            behIdxFig = openfig(expandName(behIdxFiles), 'invisible');
-        elseif numel(behIdxFiles) > 1
-            fprintf(1, "Found more than 1 file! Taking the first.\n")
-            behIdxFig = openfig(expandName(behIdxFiles(1)), 'invisible');
-        else
+        behIdxFig = arrayfun(@(x) openfig(expandName(x), 'invisible'), ...
+                behIdxFiles);
+        behRes = arrayfun(@(x) get(x, 'UserData'), behIdxFig, fnOpts{:});
+        brSz = cellfun(@numel, behRes); c = 1;
+        if numel(behIdxFiles) > 1
+            fprintf(1, "Found more than 1 file! Estimating the correct\n")
+            Ncond = arrayfun(@(m) arrayfun(@(s) numel(fieldnames(s))-1, ...
+                m.Sessions), mice, fnOpts{:}); Ncond = cat(1, Ncond{:});
+            [~, whr] = min(Ncond - brSz',[],"all"); 
+            if std(brSz)
+                [~, c] = ind2sub([numel(Ncond), numel(brSz)], whr);
+            else
+                posCCN = cellfun(@(bc) arrayfun(@(bs) ...
+                    string(bs.ConditionName), bc), behRes, fnOpts{:});
+                c = cellfun(@(nms) any(contains(nms, 'delay', ctOpts{:})), posCCN);
+            end
+            fprintf(1, "Chose the following conditions:\n")
+            fprintf(1, " - %s\n", arrayfun(@(x) string(x.ConditionName), ...
+                behRes{c}))
+        elseif isempty(behIdxFig)
             fprintf(1, "Found no BehIndex figure!\n")
             continue
         end
-        behRes = get(behIdxFig, 'UserData'); close(behIdxFig)
+        behRes = behRes{c};
+        arrayfun(@close, behIdxFig)
         consCondNames = arrayfun(@(x) string(x.ConditionName), behRes);
         % TODO: fix the condition names and run the loop
         ctrFlag = contains(consCondNames, 'control', ctOpts{:});
         delFlag = contains(consCondNames, 'delay', ctOpts{:});
+        frqFlag = ~cellfun(@isempty,regexp(consCondNames, lsOpts{:}));
         if string(oldSess) ~= string(currSess)
             oldSess = currSess; auxStruct = struct('Date', currSess, ...
                 'Control', behRes(ctrFlag).BehIndex, ...
-                'Muscimol', behRes(delFlag).BehIndex);
+                'Laser_continuous', behRes(xor(delFlag,frqFlag)).BehIndex);
+            if any(frqFlag)
+                auxStruct.Laser_freq = behRes(frqFlag).BehIndex;
+            end
             if ~isfield(mice, 'Sessions')
                 mice(mc).Sessions = auxStruct;
             else
