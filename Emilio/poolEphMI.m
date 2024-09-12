e537adefc3b23ff1626da1b7efdb3eff55cbf97f
Emilio Isaías-Camacho <emilios.isaias@gmail.com>
2024-09-12 15:11:18 +0200

    Saving logPSTH and the popLogPSTH

--- Emilio/poolEphMI.m
+++ Emilio/poolEphMI.m
@@ -28,7 +28,8 @@ animalFolders = arrayfun(@(d) recursiveFolderSearch(expandName(d), ...
     rsOpts{:}), childFolders, fnOpts{:}); animalFolders = cat(1, animalFolders{:});
 %% Looping animals
 oldMouse = "";
-mc = 0; mice = [];
+mc = 0; mice = []; lp_mu = []; lPSTH = [];
+var2save = {'mice', 'lp_mu', 'lPSTH'};
 for cad = tocol(animalFolders)'
     [structPath, currMouse] = fileparts(cad);
     [~, structName] = fileparts(structPath);
@@ -79,9 +80,10 @@ for cad = tocol(animalFolders)'
         end
         % miIdxStr = arrayfun(@(x) load( expandName(x), 'logPSTH' ), miIdxFiles);
         load( expandName(miIdxFiles), 'logPSTH' );
-        lp_mu = squeeze( mean( ...
-            logPSTH.LogPSTH(:,:,logPSTH.indexMIComparison) ) );
-        muMI = getMI( lp_mu, 2 );
+        lPSTH = cat( 1, lPSTH, {logPSTH.LogPSTH} );
+        lp_mu = cat( 3, lp_mu, squeeze( mean( ...
+            logPSTH.LogPSTH(:,:,logPSTH.indexMIComparison) ) ) );
+        muMI = getMI( lp_mu(:,:,end), 2 ); muMI(isnan( muMI )) = 0;
         bmot_MI = mean( muMI( ~(logPSTH.TimeAxis < 5e-2) ) );
         sens_MI = mean( muMI( logPSTH.TimeAxis < 5e-2) );
         miVal = [sens_MI, bmot_MI];
@@ -113,7 +115,7 @@ svOpts = {'-mat'};
 if exist(behFP, "file")
     svOpts = {'-append'};
 end
-save(behFP, "mice", svOpts{:})
+save(behFP, var2save{:}, svOpts{:})
 habFlag = arrayfun(@(m) arrayfun(@(s) string(s.Type) == "multi", ...
     m.Sessions), mice, fnOpts{:});
 cat( 1, habFlag{:} )
